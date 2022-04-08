3c0a9ec146878bc91d9bd06e7b653dff0d573692
alexmwaura <alexmwaura43@gmail.com>
2022-04-08 11:49:15 +0300

    render articles

--- view/src/App.js
+++ view/src/App.js
@@ -1,10 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { getArticles } from './client';
+import Comments from './Comments';
+import moment from 'moment';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div>Blog life</div>
-  );
+axios.defaults.baseURL = 'http://localhost:8080';
+
+class App extends Component {
+  state = {
+    articles: null,
+    isFetching: false,
+  };
+
+  componentDidMount() {
+    this.fetchArticles();
+  }
+
+  formatDate(date) {
+    return moment(date).fromNow();
+  }
+
+  fetchArticles() {
+    this.setState({
+      isFetching: true,
+    });
+    getArticles()
+      .then((res) => {
+        this.setState({
+          articles: res,
+          isFetching: false,
+        });
+      })
+      .catch((e) => {});
+  }
+
+  render() {
+    const { articles } = this.state;
+    console.log(articles);
+    if (articles) {
+      return (
+        <>
+          <div id='container'>
+            <h1 id='headline'>Blog Life</h1>
+
+            {articles.map((article) => (
+              <div key={article.id} className='article'>
+                <h2 id='subhead'>{article.title}</h2>
+                <p id='edit'>
+                  edit
+                </p>
+                <p className='dateline'>
+                  Posted: {this.formatDate(article.updatedAt)}
+                </p>
+                
+                <p>{article.description}</p>
+                <Comments
+                  comments={article.comments}
+                  formatDate={this.formatDate}
+                />
+              </div>
+            ))}
+          </div>
+        </>
+      );
+    }
+  }
 }
 
 export default App;
