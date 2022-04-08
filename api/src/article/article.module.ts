6fdfde7b6f1912393a92923674a032fa411a1e51
alexmwaura <alexmwaura43@gmail.com>
2022-04-08 10:21:10 +0300

    set typeorm and add comment module

--- api/src/article/article.module.ts
+++ api/src/article/article.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { ArticleController } from './article.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Article } from './entities/article.entity';
+import { CommentModule } from 'src/comment/comment.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Article]), CommentModule],
   controllers: [ArticleController],
-  providers: [ArticleService]
+  providers: [ArticleService],
 })
 export class ArticleModule {}
