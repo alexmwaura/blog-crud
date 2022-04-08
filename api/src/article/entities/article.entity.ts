f7d9fd656234cb0ed2715fdf93868993dcd8cf92
alexmwaura <alexmwaura43@gmail.com>
2022-04-08 10:14:59 +0300

    model schema

--- api/src/article/entities/article.entity.ts
+++ api/src/article/entities/article.entity.ts
@@ -1,1 +1,33 @@
-export class Article {}
+import {
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Comment } from 'src/comment/entities/comment.entity';
+
+@Entity()
+export class Article {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'title', type: 'varchar', nullable: false })
+  title: string;
+
+  @Column({ name: 'description', type: 'varchar', nullable: false })
+  description: string;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  @OneToMany(() => Comment, (comment) => comment.article, {
+    eager: true,
+    onDelete: 'CASCADE',
+  })
+  comments: Comment[];
+}
--- api/src/comment/entities/comment.entity.ts
+++ api/src/comment/entities/comment.entity.ts
@@ -1,1 +1,36 @@
-export class Comment {}
+import { Article } from 'src/article/entities/article.entity';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Comment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'username', type: 'varchar', nullable: false })
+  username: string;
+
+  @Column({ name: 'message', type: 'varchar', nullable: false })
+  message: string;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  @Column({ name: 'articleId', type: 'uuid', nullable: false })
+  articleId: string;
+
+  @ManyToOne(() => Article, (article) => article.comments, {
+    eager: false,
+    onDelete: 'CASCADE',
+  })
+  article: Article;
+}
