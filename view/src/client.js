5639ddc50b2368ef170284704eb3b5deac969c5b
alexmwaura <alexmwaura43@gmail.com>
2022-04-08 14:35:18 +0300

    add API call

--- view/src/client.js
+++ view/src/client.js
@@ -1,3 +1,7 @@
 import axios from 'axios';
 
 export const getArticles = () => axios.get('/article').then((res) => res.data);
+
+export const addArticle = (article) => axios.post('/article', article);
+
+export const addComment = (comment) => axios.post('/comment', comment).then((res)=>res);
