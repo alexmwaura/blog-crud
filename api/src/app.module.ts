1a683faf9c23a8184c2f349dc5094bb9b8f11e89
alexmwaura <alexmwaura43@gmail.com>
2022-04-08 11:11:28 +0300

    add modules and deps

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,19 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { ArticleModule } from './article/article.module';
 import { CommentModule } from './comment/comment.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
     }),
-    ,
     ArticleModule,
     CommentModule,
   ],
