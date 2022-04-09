d386c884350a8a3f2d7090f331290e6c661adc47
alexmwaura <alexmwaura43@gmail.com>
2022-04-09 16:24:42 +0300

    handlers

--- view/src/client.js
+++ view/src/client.js
@@ -5,3 +5,7 @@ export const getArticles = () => axios.get('/article').then((res) => res.data);
 export const addArticle = (article) => axios.post('/article', article);
 
 export const addComment = (comment) => axios.post('/comment', comment).then((res)=>res);
+
+export const editArticle = (id,article) => axios.patch(`/article/${id}`, article)
+
+export const deleteArticle = (id) => axios.delete(`/article/${id}`)
