2a4ebc776b706abe46406dddc01cf2c982a9dbdb
alexmwaura <alexmwaura43@gmail.com>
2022-04-09 19:18:35 +0300

    modal

--- view/src/Footer.js
+++ view/src/Footer.js
@@ -1,35 +1,30 @@
 import { Button, Modal } from 'antd';
 import { openNotification } from './Notification';
 import AddArticleForm from './Forms/ArticleForm';
+import {CloseOutlined} from '@ant-design/icons'
 
 const Footer = (props) => (
   <div className='footer'>
-    <Button onClick={() => props.handleAddArticle()} className='blog_button'>
-      Blog
-    </Button>
-    <Modal
-      visible={props.isAddStudentModalVisible}
-      onOk={props.closeAddModal}
-      onCancel={props.closeAddModal}
-      width={800}
-      okText='Done'
-      cancelText='Exit'
-    >
-      <AddArticleForm
-        onSuccess={() => {
-          props.closeAddModal();
-          props.fetchArticles();
-          openNotification('success', 'article added', 200);
-        }}
-        onFailure={(err) => {
-          const { message, httpStatus } = err.error;
-          openNotification('error', message, httpStatus);
-        }}
-      />
-    </Modal>
+    <input class='modal-btn' type='checkbox' id='modal-btn' name='modal-btn' />
+    <label for='modal-btn'>
+      Blog <i class='uil uil-expand-arrows'></i>
+    </label>
+    <div class='modal'>
+      <div class='modal-wrap'>
+        <AddArticleForm
+          onSuccess={() => {
+            props.closeAddModal();
+            props.fetchArticles();
+            openNotification('success', 'article added', 200);
+          }}
+          onFailure={(err) => {
+            const { message, httpStatus } = err.error;
+            openNotification('error', message, httpStatus);
+          }}
+        />
+      </div>
+    </div>
   </div>
 );
 
 export default Footer;
-
-
